8ccaabd3b01bff65f00d456257ac07ff65066dc2
Perry Angelora <peamaillist@gmail.com>
2020-10-11 17:08:49 -0400

    Added menu/article component linkage.

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SectionMenu from "./components/SectionMenu";
 
 function App() {
   return (
     <div>
-      <p>Hello y'all</p>
+      <SectionMenu />
     </div>
   );
 }
