97216794de291e102d3ad6515cab2d0b101f730a
Perry Angelora <peamaillist@gmail.com>
2020-11-15 22:09:04 -0500

    Changed sections from array of arrays to array of objects

--- frontend/app/components/SectionMenu.js
+++ frontend/app/components/SectionMenu.js
@@ -4,52 +4,57 @@ Article.js, embedded, recives section props from a selection.
 It seems a little easier to have this arangement, rather than having compaonents at the same level.
  */
 
-import React, { useState } from 'react'
-import Articles from './Articles'
+import React, { useState } from "react";
+import Articles from "./Articles";
 
-export default function SectionMenu(){
-    const [section, setSection] = useState("home");
+export default function SectionMenu() {
+  const [section, setSection] = useState("home");
 
-    const sections = [
-        ["arts","Arts"],
-        ["automobiles","Automobiles"],
-        ["books","Books"],
-        ["business","Business"],
-        ["fashion","Fashion"],
-        ["food","Food"],
-        ["health","Health"],
-        ["home","Home"],
-        ["insider","Times Insider"],
-        ["magazine","Magazine"],
-        ["movies","Movies"],
-        ["nyregion","NY Region"],
-        ["obituaries","Obituaries"],
-        ["opinion","Opinion"],
-        ["politics","Politics"],
-        ["realestate","Real Estate"],
-        ["science","Science"],
-        ["sports","Sports"],
-        ["sundayreview","Sunday Review"],
-        ["technology", "Technology"],
-        ["theater","Theater"],
-        ["t-magazine","T-Magazine"],
-        ["travel","Travel"],
-        ["upshot","Upshot"],
-        ["us","U.S."],
-        ["world","World"],
-      ];
+  const sections = [
+    { linkName: "arts", displayName: "Arts" },
+    { linkName: "automobiles", displayName: "Automobiles" },
+    { linkName: "books", displayName: "Books" },
+    { linkName: "business", displayName: "Business" },
+    { linkName: "fashion", displayName: "Fashion" },
+    { linkName: "food", displayName: "Food" },
+    { linkName: "health", displayName: "Health" },
+    { linkName: "home", displayName: "Home" },
+    { linkName: "insider", displayName: "Times Insider" },
+    { linkName: "magazine", displayName: "Magazine" },
+    { linkName: "movies", displayName: "Movies" },
+    { linkName: "nyregion", displayName: "NY Region" },
+    { linkName: "obituaries", displayName: "Obituaries" },
+    { linkName: "opinion", displayName: "Opinion" },
+    { linkName: "politics", displayName: "Politics" },
+    { linkName: "realestate", displayName: "Real Estate" },
+    { linkName: "science", displayName: "Science" },
+    { linkName: "sports", displayName: "Sports" },
+    { linkName: "sundayreview", displayName: "Sunday Review" },
+    { linkName: "technology", displayName: "Technology" },
+    { linkName: "theater", displayName: "Theater" },
+    { linkName: "t-magazine", displayName: "T-Magazine" },
+    { linkName: "travel", displayName: "Travel" },
+    { linkName: "upshot", displayName: "Upshot" },
+    { linkName: "us", displayName: "U.S." },
+    { linkName: "world", displayName: "World" },
+  ];
 
-      return (
-         <div className="menu-wrapper">
-             <ul>
-                 {sections.map((section, index)=>(
-                     <li key={index} onClick={()=>{setSection(section[0])}} style={{cursor:"pointer"}}>
-                         {section[1]}
-                     </li>
-                 ))}
-             </ul>
-             <Articles section={section}/>
-         </div>
-      )
-
+  return (
+    <div className="menu-wrapper">
+      <ul>
+        {sections.map(({ linkName, displayName }, index) => (
+          <li
+            key={index}
+            onClick={() => {
+              setSection(linkName);
+            }}
+            style={{ cursor: "pointer" }}
+          >
+            {displayName}
+          </li>
+        ))}
+      </ul>
+      <Articles section={section} />
+    </div>
+  );
 }
