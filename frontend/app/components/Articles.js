70f8454c74f96b598f9dea13adc2d8633faee042
Perry Angelora <peamaillist@gmail.com>
2020-11-15 22:07:55 -0500

    Added facets to Articles

--- frontend/app/components/Articles.js
+++ frontend/app/components/Articles.js
@@ -1,24 +1,70 @@
 import React, { useState, useEffect } from "react";
 import { grabTopStories } from "./api";
+import ModalSelectionLogin from "./ModalSelectionLogin";
 
 export default function Articles({ section }) {
   const [topStoriesData, setTopStoriesData] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [modalFacets, setModalFacets] = useState({});
+
   useEffect(() => {
     grabTopStories(section).then((dataPromise) => {
       setTopStoriesData(dataPromise);
     });
   }, [section]);
 
+  const handleModalClose = () => {
+    setShowModal(false);
+    setModalFacets({});
+  };
+
   return (
     <div className="article-wrapper">
+      <ModalSelectionLogin modalFacets={modalFacets} closeModal={handleModalClose} />
       <ul>
-        {topStoriesData.map(({ title, abstract, multimedia, url }, index) => (
-          <li key={index} onClick={ () => { window.open(url, "_blank") } }>
-            <h2>{ title }</h2>
-            <p>{ abstract }</p>
-            <img src={ multimedia ? multimedia[1].url : "" }></img>
-          </li>
-        ))}
+        {topStoriesData.map(
+          (
+            {
+              title,
+              abstract,
+              multimedia,
+              url,
+              byline,
+              des_facet,
+              org_facet,
+              per_facet,
+              geo_facet,
+            },index) => (
+            <li key={index}>
+              <div
+                onClick={() => {
+                  window.open(url, "_blank");
+                }}>
+                <h2>{title}</h2>
+                <p>{abstract}</p>
+                <img src={multimedia ? multimedia[1].url : ""}></img>
+              </div>
+              <div className="modal-button">
+                <button
+                  className="add-reading-alert"
+                  onClick={() => {
+                    setShowModal(true) ? showModal !== true : null;
+                    setModalFacets(
+                      { byline: byline ,
+                       des_facet: des_facet ,
+                      org_facet: org_facet ,
+                      per_facet: per_facet ,
+                      geo_facet: geo_facet,
+                      title }
+                    );
+                  }}
+                >
+                  +
+                </button>
+              </div>
+            </li>
+          )
+        )}
       </ul>
     </div>
   );
