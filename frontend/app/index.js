cc502c5f3a055b661a6f0a55d55eec7e9e5d6e4f
Perry Angelora <peamaillist@gmail.com>
2020-11-22 14:08:14 -0500

    Added <Router> wrap to allow useLocation hook

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -1,19 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter as Router, Route} from "react-router-dom"
-import SectionMenu from "./components/SectionMenu"
-import LoginDialog from "./components/LoginDialog"
-import UserInfo from "./components/UserInfo"
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./components/App"
 
-function App() {
-  return (
-    <Router>
-      <Route exact path="/" component={SectionMenu} />
-      <Route exact path="/login" component={LoginDialog} />
-      <Route exact path="/user-info" component={UserInfo} />
-    </Router>
-  );
-}
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  rootElement
+);
