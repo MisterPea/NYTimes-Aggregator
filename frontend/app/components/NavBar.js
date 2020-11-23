6660aa399993266c2c120073490edf0f4a5f8235
Perry Angelora <peamaillist@gmail.com>
2020-11-22 22:28:22 -0500

    Moved into the top of the tree rather than have it loaded as a à-la-carte component

--- frontend/app/components/NavBar.js
+++ frontend/app/components/NavBar.js
@@ -1,19 +1,47 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import firebase from "./Auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Link, useLocation } from "react-router-dom";
+import uidContextProvider from "./UidContext";
 
-export default function NavBar(props) {
-  const path = window.location.pathname;
-  const [auth, setAuth] = useState(null);
+export default function NavBar() {
+  const [uid, setUid] = useState(null);
+  const [username, setUsername] = useState(null);
+  const [user] = useAuthState(firebase.auth());
+  const auth = firebase.auth();
+  let path = useLocation().pathname;
+  const { setUidContext } = useContext(uidContextProvider);
+ 
+  useEffect(() => {
+    if (user) {
+      setUid(user.uid);
+      setUsername(user.displayName);
+      setUidContext(user.uid)
+    }
+  }, [user]);
+
+  const signOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        setUid(null);
+        setUidContext(null)
+        console.log("Signed out");
+        // unmount modal - flush context
+      })
+      .catch((err) => {
+        console.error(`Oh snap! A ${err} error has been thrown on sign-out.`);
+      });
+  };
 
   return (
     <div className="navbar-wrapper">
       <div className="site-title">NYT Aggregator</div>
       <div className="navigation">
-        {path !== "/" 
-        ? <Link to="/">Home</Link>
-        : auth 
-        ? <Link to="/user-info">User Info</Link>
-        : <Link to="/login">Login</Link>}
+        {path !== "/" && <Link to="/">Home</Link>}
+        {path !== "/login" && path !== "/sign-up" && !uid && <Link to="/login">Login</Link>}
+        {path !== "/user-info" && uid && <Link to="/user-info">{username}</Link>}
+        {uid && <button onClick={signOut}>Logout</button>}
       </div>
     </div>
   );
