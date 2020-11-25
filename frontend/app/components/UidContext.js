2a455cdb9ec4db9915a61e416c9f49157a182fb8
Perry Angelora <peamaillist@gmail.com>
2020-11-24 21:11:26 -0500

    Moved to /api folder

--- frontend/app/components/UidContext.js
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react'
-
-const uidContextProvider = createContext({
-    uidContext: null,
-    setUidContext: () => {}
-})
-
-export default uidContextProvider
--- frontend/app/components/VerifyEmailPassword.js
+++
@@ -1,8 +0,0 @@
-export default function VerifyEmailAndPassword(email, password) {
-  // valid password: 8 Characters with 1 Capitalized Letter and 1 Number
-  const eRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  const pRegex = /(?=[A-Za-z0-9@#$%^&+!=]+$)^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,}).*$/;
-
-  return eRegex.test(email) && pRegex.test(password)
-}
-
