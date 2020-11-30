e367979ed97f8580b59efbf025c76f62868ecfe4
Perry Angelora <peamaillist@gmail.com>
2020-11-30 18:01:06 -0500

    Added Promise to user initiation; added get method

--- frontend/app/components/api/DatabaseActions.js
+++ frontend/app/components/api/DatabaseActions.js
@@ -1,13 +1,51 @@
-import firebase from './Auth'
+/* 
+InitUser: Creates a space on the database tied to the User Id (uid)
+This method takes in a uid and returns a Promise.
 
-const db = firebase.firestore()
+AddToUser: ****** need to change to array methods
+*/
 
-export function AddToUser(uid,selection){
-    db.collection("users").doc(uid).set({selections:selection})
-    .then(()=>{
-        console.log(`${selection} added with ID:`)
+import firebase from "./Auth";
+const db = firebase.firestore();
+const userCollection = db.collection("users");
+
+export function InitUser(uid) {
+  return new Promise((resolve, reject) => {
+    userCollection.doc(uid).set({
+        active: true,
+        selections: [],
+      })
+      .then(() => {
+        resolve("Account created");
+      })
+      .catch((error) => {
+        reject(`Something went wrong: ${error}`);
+      });
+  });
+}
+
+export function AddToUser(uid, selection) {
+  userCollection.doc(uid).set({ selections: selection })
+    .then(() => {
+      console.log(`${selection} added with ID:`);
     })
-    .catch((error)=> {
-        console.error(`Error adding ${selection} to ${uid}: ${error}`)
-    })
+    .catch((error) => {
+      console.error(`Error adding ${selection} to ${uid}: ${error}`);
+    });
+}
+
+export function GetCurrentSubscriptions(uid) {
+  return new Promise((resolve, reject) => {
+    userCollection.doc(uid).get()
+      .then((doc) => {
+        if (doc.exists) {
+          resolve(doc.data().selections)
+        } else {
+          reject("doc doesn't exist")
+        }
+      })
+      .catch((error) => {
+        reject(`Error getting current subscriptions. Code:${error}`)
+      })
+  });
 }
