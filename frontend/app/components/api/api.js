e0fe1ead8767905a7e0399f2fbdec3fd8af703f5
Perry Angelora <peamaillist@gmail.com>
2020-12-05 19:02:55 -0500

    Added JS Docs

--- frontend/app/components/api/api.js
+++ frontend/app/components/api/api.js
@@ -1,11 +1,10 @@
 const key = 'nDByaIg6JlesybbNCdNId8mQ7ScGOKCD' // This will be moved to the backend - will request new key at that time.
 
-/* 
-This function makes api calls to their TopStories API
-@ arg {String} section - String representation of the section to be pulled. 
-@ return {Promise} data.results - return is a promise of the resultant api return.
+/**
+ * Method to pull stories from NY Times TopStories API.
+ * @param {string} section - String representation of the section to be pulled.
+ * @return {Promise} Top Stories data.
 */
-
 export function grabTopStories(section){
     return fetch(`https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${key}`)
         .then(result => result.json())
