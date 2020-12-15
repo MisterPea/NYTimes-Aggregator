88e4b831e78868961e4c94e25708f273eea90868
Perry Angelora <peamaillist@gmail.com>
2020-12-15 14:15:49 -0500

    Added doc string

--- frontend/app/components/api/VerifyEmailPassword.js
+++ frontend/app/components/api/VerifyEmailPassword.js
@@ -1,6 +1,12 @@
-// valid password: 8 Characters with 1 Capitalized Letter and 1 Number
-// valid username: Between 3-35 characters including -_& A-Z a-z
-
+/**
+ * Checking method for name, email and password; collectively checks all three as a unit.
+ * Valid password: 8 Characters with 1 Capitalized Letter and 1 Number
+ * Valid username: Between 3-35 characters including -_& A-Z a-z
+ * @param {string} user
+ * @param {string} userEmail
+ * @param {string} userPassword
+ * @returns {boolean}
+  */
 export function VerifyEmailAndPassword(user = null, userEmail = null, userPassword = null) {
   const eRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const pRegex = /(?=[A-Za-z0-9@#$%^&+!=]+$)^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,}).*$/;
@@ -19,8 +25,25 @@ export function VerifyEmailAndPassword(user = null, userEmail = null, userPasswo
   };
 }
 
+/**
+ * Verifies validity of an entered email.
+ * @param {string} userEmail
+ * @returns {boolean}
+ */
 export function VerifyEmail(userEmail) {
   const eRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const email = eRegex.test(userEmail);
   return email;
+}
+
+/**
+ * Verifies the validity of an entered password:
+ * 8 Characters with 1 Capitalized Letter and 1 Number.
+ * @param {string} userPassword
+ * @returns {boolean}
+ */
+export function VerifyPassword(userPassword){
+  const pRegex = /(?=[A-Za-z0-9@#$%^&+!=]+$)^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,}).*$/;
+  const password = pRegex.test(userPassword)
+  return password
 }
