ed5cb044dc6c1ca4f552f4e80a92a4b443de4d09
Perry Angelora <peamaillist@gmail.com>
2021-02-01 11:51:22 -0500

    Added getUniqueSelections method - changed name of module

---
+++ backend/functions/components/userAcctMehods.js
@@ -0,0 +1,68 @@
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+const db = admin.firestore();
+const auth = admin.auth();
+
+const usersCollection = db.collection("users");
+
+/**
+ * Asynchronous Method to get all user information.
+ * @param {func} callback - A function to pass output to.
+ * @return {func} callback - Formatted as an Array of Objects.
+ * - active: {Bool} Account is currently reciveing email updates.
+ * - selections: {Array} selections/subscription keywords
+ * - email: {String} Users email
+ */
+async function getUserInfo(callback) {
+  const userPromiseArray = [];
+
+  try {
+    const users = await usersCollection.get();
+    users.forEach((user) => {
+      const {active, selections} = user.data();
+      const activeP = Promise.resolve(active);
+      const selectionsP = Promise.resolve(selections);
+      const emailP = auth.getUser(user.id).then((id) => {
+        return id.email;
+      });
+      userPromiseArray.push([activeP, selectionsP, emailP]);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+
+  const result = [];
+  for (let i = 0; i < userPromiseArray.length; i++) {
+    const userPromise = userPromiseArray[i];
+    Promise.all(userPromise)
+        .then((userData) => {
+          result.push({
+            active: userData[0],
+            selections: userData[1],
+            email: userData[2],
+          });
+          if (i === userPromiseArray.length - 1) {
+            callback(result);
+          }
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+  }
+}
+
+/**
+ * Method to get all unique selections.
+ * @param {Array.<Object>} userInfo Array of Objects derived from getUserInfo()
+ * @return {Set} Returns a set of unique selections
+ */
+function getUniqueSelections(userInfo) {
+  const selectionsSet = new Set();
+  for (const user of userInfo) {
+    const {selections} = user;
+    selections.forEach(selectionsSet.add, selectionsSet);
+  }
+  return selectionsSet;
+}
+module.exports = {getUserInfo, getUniqueSelections};
