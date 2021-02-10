bbbe5c09f67c18445ecf797b61ff66519b35d2e7
Perry Angelora <peamaillist@gmail.com>
2021-02-10 10:36:27 -0500

    Moved api call from client to server

--- frontend/app/components/api/api.js
+++ frontend/app/components/api/api.js
@@ -1,4 +1,4 @@
-const key = 'nDByaIg6JlesybbNCdNId8mQ7ScGOKCD' // This will be moved to the backend - will request new key at that time.
+// const key = 'nDByaIg6JlesybbNCdNId8mQ7ScGOKCD' // This will be moved to the backend - will request new key at that time.
 
 /**
  * Method to pull stories from NY Times TopStories API.
@@ -6,14 +6,15 @@ const key = 'nDByaIg6JlesybbNCdNId8mQ7ScGOKCD' // This will be moved to the back
  * @return {Promise} Top Stories data.
 */
 export function grabTopStories(section){
-    return fetch(`https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${key}`)
+    return fetch(`http://localhost:8090/${section}`)
         .then(result => result.json())
         .then(data => {
             if(!data.results){
                 throw new Error(data.message)
             }
             return data.results 
-        })  
+        }) 
+        .catch((err) => console.error(err))
 }
 
 
