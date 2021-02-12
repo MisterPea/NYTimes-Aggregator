8010761644e78817882f59c83dbd1b82a8cbcffb
Perry Angelora <peamaillist@gmail.com>
2021-02-12 12:07:00 -0500

    Minior simplifying

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const { request, response } = require("express");
 require("dotenv").config();
 
 const app = express();
@@ -9,7 +8,6 @@ const port = 8090;
 app.use(cors());
 
 const nyt_api = require("./routes/nyt_api");
-
 app.use("/", nyt_api);
 
 app.listen(port, () => {
