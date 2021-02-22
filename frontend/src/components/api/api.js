b02c9db342a62cb83beae122e7240792dee10fe5
Perry Angelora <peamaillist@gmail.com>
2021-02-22 15:27:21 -0500

    Deployment testing

--- frontend/src/components/api/api.js
+++ frontend/src/components/api/api.js
@@ -3,10 +3,10 @@
  * @param {string} section - String representation of the section to be pulled.
  * @return {Promise} Top Stories data.
 */
-export function grabTopStories(section){
+export function grabTopStories(/* section */){
     console.log("called")
-    return fetch(`http://localhost/api/${section}`)
-    // return fetch(`http://localhost/api`)
+    // return fetch(`http://localhost/api/${section}`)
+    return fetch(`http://localhost/api`)
         .then(result => result.json())
         .then(data => {
             if(!data.results){
