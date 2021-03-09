08cc2bb2a2cdc5a8d0a1c0ecbbd9e5724daea1d6
Perry Angelora <peamaillist@gmail.com>
2021-03-08 22:59:58 -0500

    Link to main stylesheet

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import App from "./components/App"
+import App from "./components/App";
+import "../style/style_main.scss"
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
