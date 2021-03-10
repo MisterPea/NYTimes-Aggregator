9625c956e01b463fe9321e5767e9ef379d243691
Perry Angelora <peamaillist@gmail.com>
2021-03-09 23:01:05 -0500

    Added box-shadow to menu bar

--- frontend/src/components/HortizonalScrollMenu.js
+++ frontend/src/components/HortizonalScrollMenu.js
@@ -8,8 +8,8 @@ import PropTypes from "prop-types";
 const StyledAppBar = withStyles({
   root: {
     position: "fixed",
-    boxShadow: "none",
     backgroundColor: "rgb(0,0,0)",
+    boxShadow: "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px"
   },
 })(AppBar);
 
