553cc1c1cff47ce18c27a09ce2d69468e3f46fbb
Perry Angelora <peamaillist@gmail.com>
2021-03-15 12:25:50 -0400

    Moved login from dedicated page to dialog - added handling for login attempts from other pages

--- frontend/src/components/Articles.js
+++ frontend/src/components/Articles.js
@@ -1,18 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useLayoutEffect } from "react";
 import PropTypes from 'prop-types'
 import { grabTopStories } from "./api/api";
 import ModalSelectionLogin from "./ModalSelectionLogin";
 import AddCircleIcon from '@material-ui/icons/AddCircle';
 import { Dialog } from '@material-ui/core';
 import { Slide } from '@material-ui/core';
+import { useLocation } from "react-router-dom";
+
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="right" ref={ref} {...props} />;
 })
 
 export default function Articles({ section }) {
+  const { state } = useLocation();
+  const openLogin = state ? state.locationFrom : false
   const [topStoriesData, setTopStoriesData] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const [modalFacets, setModalFacets] = useState({});
+  const [mapCompleted, setMapCompleted] = useState(false);
 
   useEffect(() => {
     // The isMounted flag is to prevent update on an unmounted component. 
@@ -24,15 +29,33 @@ export default function Articles({ section }) {
       .catch(err => {
         console.error(`There's been an error fetching NY Times's Top Stories: ${err}`)
       })
+    
     return () => {
       isMounted = false;
     };
   }, [section]);
 
+  useLayoutEffect(() => {
+    if(topStoriesData !== []){
+      if(mapCompleted === true){
+      openLogin === true && setShowModal(true) 
+      }
+    }
+  },[mapCompleted]);
+
   const handleModalClose = () => {
     setShowModal(false);
     setModalFacets({});
+    window.history.replaceState({locationFrom:false},null)
   };
+
+  const trackMapCompletion = (index, length) => {
+    if(index === length - 1) {
+      if(mapCompleted === false) {
+        setMapCompleted(true);
+      }
+    }
+  }
 
   return (
     <div className="article-wrapper">
@@ -41,7 +64,7 @@ export default function Articles({ section }) {
       onClose={handleModalClose}
       TransitionComponent={Transition}
     >
-      <ModalSelectionLogin modalFacets={modalFacets} closeModal={handleModalClose} />
+      <ModalSelectionLogin modalFacets={modalFacets} message={state ? "articles" : ""} closeModal={handleModalClose} />
     </Dialog>
       <ul>
         {topStoriesData.map(
@@ -58,8 +81,7 @@ export default function Articles({ section }) {
               org_facet,
               per_facet,
               geo_facet,
-            },
-            index
+            },index,wholeArray
           ) => (
             <li className="article-list-item" key={index}>
               <p className="section-text">{`${section} ${subsection === "" ? "" : "• " + subsection}`}</p>
@@ -92,6 +114,7 @@ export default function Articles({ section }) {
               <div className="shadow-holder"  onClick={() => {
                   window.open(url, "_blank");
                 }}></div>
+                {trackMapCompletion(index, wholeArray.length)}
             </li>
           )
         )}
@@ -101,5 +124,5 @@ export default function Articles({ section }) {
 }
 
 Articles.propTypes = {
-  section: PropTypes.string
+  section: PropTypes.string,
 }
