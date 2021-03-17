29730fa269f43447e0057e118e895d2c6739ce70
Perry Angelora <peamaillist@gmail.com>
2021-03-16 21:44:41 -0400

    Added Material-UI media query

--- frontend/src/components/UserInfo.js
+++ frontend/src/components/UserInfo.js
@@ -2,14 +2,12 @@ import React, { useState } from "react";
 import AccountInfo from "./userInfo/AccountInfo";
 import SubscriptionInfo from "./userInfo/SubscriptionInfo";
 import { ButtonGroup, Button } from "@material-ui/core";
-import { makeStyles } from "@material-ui/styles"
+import { makeStyles } from "@material-ui/styles";
+import { useMediaQuery } from "@material-ui/core"
 
 export default function UserInfo() {
-  const [currentPanel, setCurrentPanel] = useState("sub");
+  const doubleWide = useMediaQuery('(min-width: 960px)');
 
-  const handleClick = (btn) => {
-    setCurrentPanel(btn);
-  };
 
   const useStyles = makeStyles({
     root: {
@@ -37,32 +35,63 @@ export default function UserInfo() {
     }
   });
 
+  
   const classes = useStyles();
-  // const {root, button} = useStyles();
+
+  const DoubleWide = () => {
+    return (
+      <div className="control-panel">
+        <div className="info-holder">
+          <AccountInfo />
+        </div>
+        <div className="info-holder">
+          <SubscriptionInfo />
+        </div>
+      </div>
+    );
+  }
+
+  const SingleWide = () => {
+    const [currentPanel, setCurrentPanel] = useState("sub");
+
+    const handleClick = (btn) => {
+      setCurrentPanel(btn);
+    };
+    
+    return (
+      <React.Fragment>
+        <div className="control-panel">
+          <div className={classes.root}>
+            <ButtonGroup variant="contained" fullWidth={true}>
+              <Button
+                className={classes.button}
+                disabled={currentPanel === "sub"}
+                onClick={() => {
+                  handleClick("sub");
+                }}>
+                Subscription Info
+              </Button>
+              <Button
+                className={classes.button}
+                disabled={currentPanel === "info"}
+                onClick={() => {
+                  handleClick("info");
+                }}>
+                Account Info
+              </Button>
+            </ButtonGroup>
+          </div>
+          <div className="info-holder">
+            {currentPanel === "info" ? <AccountInfo /> : <SubscriptionInfo />}
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  };
 
   return (
-    <div className="control-panel">
-      <div className={classes.root}>
-        <ButtonGroup  variant="contained" fullWidth={true}>
-          <Button
-            className={classes.button}
-            disabled={currentPanel === "sub"} 
-            onClick={() => {handleClick("sub")}}
-            >
-            Subscription Info
-          </Button>
-          <Button
-            className={classes.button}
-            disabled={currentPanel === "info"}
-            onClick={() => {handleClick("info")}}
-            >
-            Account Info
-          </Button>
-        </ButtonGroup>
-        </div>
-      <div className="info-holder">
-        {currentPanel === "info" ? <AccountInfo /> : <SubscriptionInfo />}
-      </div>
-    </div>
-  );
+    <React.Fragment >
+      {doubleWide === true ? <DoubleWide /> : <SingleWide />}
+    </React.Fragment>
+  )
 }
