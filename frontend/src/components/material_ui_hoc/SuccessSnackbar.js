bc6eedbacc499ebccd7a3c38a38cfde68f034d75
Perry Angelora <peamaillist@gmail.com>
2021-03-18 23:24:14 -0400

    Initial commit

---
+++ frontend/src/components/material_ui_hoc/SuccessSnackbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Snackbar } from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
+import PropTypes from "prop-types";
+
+function SuccessSnackbar({ message, onOpen, onClose }) {
+  return (
+    <Snackbar
+      open={onOpen}
+      onClose={onClose}
+      anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+      autoHideDuration={5000}
+      >
+      <Alert onClose={onClose} severity="success" variant="filled">
+        {`Welcome back, ${message}`}
+      </Alert>
+    </Snackbar> 
+  );
+}
+SuccessSnackbar.propTypes = {
+  message: PropTypes.string,
+  onOpen: PropTypes.bool,
+  onClose: PropTypes.func,
+  classes: PropTypes.object,
+  root: PropTypes.object,
+};
+
+export default SuccessSnackbar
