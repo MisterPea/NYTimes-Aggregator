d00a841991eeb29323d0634abdf46ad143ce9e22
Perry Angelora <peamaillist@gmail.com>
2022-02-07 22:07:09 -0500

    Added filter to remove empty articles

--- frontend/src/components/api/api.js
+++ frontend/src/components/api/api.js
@@ -1,4 +1,21 @@
 /**
+ * Method to remove empty elements/articles from the results array
+ * @param {Array} data Array of articles
+ * @returns Returns an array of articles
+ */
+function checkForEmptyData(data) {
+  const sanitizedData = [];
+  for (let i = 0; i < data.length; i += 1) {
+    const article = data[i];
+    const { section, title, abstract } = article;
+    if (section.length !== 0 && title.length !== 0 && abstract.length !== 0) {
+      sanitizedData.push(article);
+    }
+  }
+  return sanitizedData;
+}
+
+/**
  * Method to pull stories from NY Times TopStories API.
  * @param {string} section - String representation of the section to be pulled.
  * @return {Promise} Top Stories data.
@@ -10,7 +27,7 @@ export default function grabTopStories(section) {
       if (!data) {
         throw new Error(data.message);
       }
-      return data;
+      return { ...data, results: checkForEmptyData(data.results) };
     })
     .catch((err) => console.error('Failed', err));
 }
