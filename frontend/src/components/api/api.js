aa1d0c5c0fd607afc57b21af2c67dc218a81f128
Perry Angelora <peamaillist@gmail.com>
2022-03-24 14:24:29 -0400

    Updated fetch route

--- frontend/src/components/api/api.js
+++ frontend/src/components/api/api.js
@@ -21,7 +21,7 @@ function checkForEmptyData(data) {
  * @return {Promise} Top Stories data.
 */
 export default function grabTopStories(section) {
-  return fetch(`http://nytimes-aggregator_backend_1/${section}`)
+  return fetch(`https://thetimespage.misterpea.me/api/${section}`)
     .then((result) => result.json())
     .then((data) => {
       if (!data) {
